d898d9bd586542a34c294b490fe670458ff5f63d
2ThuanCr <2thuancr03@gmail.com>
2025-04-22 23:23:24 +0700

    Add initial adapter and layout files for banners and categories, and update drawable resources

---
+++ app/src/main/java/com/student22110006/fashionshop/data/model/product/Category.java
@@ -0,0 +1,4 @@
+package com.student22110006.fashionshop.data.model.product;
+
+public class Categort {
+}
---
+++ app/src/main/java/com/student22110006/fashionshop/ui/home/SpaceItemDecoration.java
@@ -0,0 +1,4 @@
+package com.student22110006.fashionshop.ui.home;
+
+public class SpaceItemDecoration {
+}
