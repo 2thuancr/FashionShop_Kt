c7e89e8a6c8aed5633178197aa17d36a1d5b9c82
2ThuanCr <2ThuanCr03@gmail.com>
2025-04-26 00:48:36 +0700

    Add sale badge for product

--- app/src/main/java/com/student22110006/fashionshop/adapter/ListProductAdapter.java
+++ app/src/main/java/com/student22110006/fashionshop/adapter/ListProductAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,15 +46,20 @@ public class ListProductAdapter extends RecyclerView.Adapter<ListProductAdapter.
 
         // Set giá trị cho các thành phần giao diện
         holder.textProductName.setText(product.getName());
+        holder.textProductDescription.setText(product.getDescription());
         holder.textProductPrice.setText(product.getPrice() + "đ");
 
         Glide.with(holder.itemView.getContext())
                 .load(product.getImageUrl())
                 .into(holder.imageProduct);
 
-
-        // TODO thêm sự kiện button Click
-
+        // Hiển thị badge giảm giá nếu có
+        if (product.getDiscount() > 0) {
+            holder.textSaleBadge.setVisibility(View.VISIBLE);
+            holder.textSaleBadge.setText("-" + product.getDiscount() + "%");
+        } else {
+            holder.textSaleBadge.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -61,20 +67,34 @@ public class ListProductAdapter extends RecyclerView.Adapter<ListProductAdapter.
         return productList.size();
     }
 
-    class ProductViewHolder extends RecyclerView.ViewHolder{
+    class ProductViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageProduct;
         private TextView textProductName;
+        private TextView textProductDescription;
         private TextView textProductPrice;
+        private TextView textSaleBadge;
         private Button buttonAddToCart;
 
         public ProductViewHolder(View itemView) {
             super(itemView);
             imageProduct = (ImageView) itemView.findViewById(R.id.imageProduct);
             textProductName = (TextView) itemView.findViewById(R.id.textProductName);
+            textProductDescription = (TextView) itemView.findViewById(R.id.textProductDescription);
             textProductPrice = (TextView) itemView.findViewById(R.id.textProductPrice);
+            textSaleBadge = (TextView) itemView.findViewById(R.id.textSaleBadge);
             buttonAddToCart = (Button) itemView.findViewById(R.id.buttonAddToCart);
 
             itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Lấy ra item product tại vị trí position
+                    Product product = productList.get(getAdapterPosition());
+                    // Hiển thị thông báo sản phẩm được chọn
+                    Toast.makeText(context, product.getName(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            buttonAddToCart.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     // Lấy ra item product tại vị trí position
