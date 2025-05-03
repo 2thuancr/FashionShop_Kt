c0fe67cd86cabbfc0f12bbd9b2a3dde2d7c7631d
2ThuanCr <2ThuanCr03@gmail.com>
2025-05-03 17:53:45 +0700

    Init AddressListFragment, AddEditAddressFragment

---
+++ app/src/main/java/com/student22110006/fashionshop/ui/profile/AddEditAddressViewModel.java
@@ -0,0 +1,7 @@
+package com.student22110006.fashionshop.ui.profile;
+
+import androidx.lifecycle.ViewModel;
+
+public class AddEditAddressViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
