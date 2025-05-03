f5656932a3c10494ce9f703046da49c1eca1ba04
2ThuanCr <2ThuanCr03@gmail.com>
2025-05-03 15:54:20 +0700

    Init NotificationDetailFragment

---
+++ app/src/main/java/com/student22110006/fashionshop/ui/notifications/NotificationDetailViewModel.java
@@ -0,0 +1,7 @@
+package com.student22110006.fashionshop.ui.notifications;
+
+import androidx.lifecycle.ViewModel;
+
+public class NotificationDetailViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
